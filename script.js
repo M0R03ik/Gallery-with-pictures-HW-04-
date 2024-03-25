10ff41672bdc85a81d2dd4d758b2cf30f17ad24f
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-03-25 14:41:37 +0300

    Добавил лоадер. Настроил адаптив

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('.button')
 const buttonMoore = document.querySelector('.button_moore')
+const loader = document.querySelector('.loader')
 
 button.addEventListener('click', () => {
   getContent().then(() => (buttonMoore.style.display = 'block'))
@@ -9,6 +10,7 @@ buttonMoore.addEventListener('click', getContent)
 
 async function getContent() {
   try {
+    showLoader()
     let promise = await fetch('https://dog.ceo/api/breeds/image/random/20')
     if (!promise.ok) {
       throw new Error('Произошла ошибка')
@@ -18,6 +20,8 @@ async function getContent() {
     createImage(data)
   } catch (e) {
     console.error(e)
+  } finally {
+    hideLoader()
   }
 }
 
@@ -29,4 +33,12 @@ function createImage(images) {
     image.src = url
     gallery.appendChild(image)
   }
+}
+
+function showLoader() {
+  loader.style.display = 'inline-block'
+}
+
+function hideLoader() {
+  loader.style.display = 'none'
 }
